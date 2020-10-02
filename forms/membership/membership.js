c40b824b39cca3f5a8b66c67fc0e59dcbad11742
mleider <mckenzieleider@creighton.edu>
2020-10-02 12:01:57 -0500

    commiting changes so far
    adding the membership for fixit 8

--- forms/membership/membership.js
+++ forms/membership/membership.js
@@ -1,4 +1,11 @@
-/*
-let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
-*/
-
+let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"];
+let firstName = "";
+btnSubmitMember.onclick=function(){
+  firstName = inptName.value
+  if(members.includes(firstName))
+    lblMembershipForm.textContent = 'You are a member!'
+  else
+    lblMembershipForm.textContent = 'You are not a member, let me add you!'
+    members.push(firstName);
+    console.log(members);
+}
